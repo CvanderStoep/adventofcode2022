cd62f65ed2bd364fbb4522ec8dd27199cabafb5c
carlo <carlo.vanderstoep@gmail.com>
2022-12-09 14:41:57 +0100

    day 3 & 4

--- day4.py
+++ day4.py
@@ -10,22 +10,28 @@ def read_input_file():
         content = f.read().splitlines()
 
     number_of_overlaps = 0
+    number_of_overlapping_assignments = 0
     for item in content:
         sections = item.split(',')
         section1, section2 = sections[0], sections[1]
         ranges = section1.split('-')
-        section1_left, section1_right = int(ranges[0]), int(ranges[1])
+        L1, R1 = int(ranges[0]), int(ranges[1])
         ranges = section2.split('-')
-        section2_left, section2_right = int(ranges[0]), int(ranges[1])
+        L2, R2 = int(ranges[0]), int(ranges[1])
 
-        if (section1_left <= section2_left and section2_right <= section1_right) or \
-                (section2_left <= section1_left and section1_right <= section2_right):
+        # part I
+        if (L1 <= L2 and R2 <= R1) or (L2 <= L1 and R1 <= R2):
             number_of_overlaps += 1
+
+        # part II
+        if (L2 <= L1 <= R2) or (L2 <= R1 <= R2) or (L1 <= L2 <= R1) or (L1 <= R2 <= R1):
+            number_of_overlapping_assignments += 1
 
     # part I
     print('partI: number of fully overlapping ranges= ', number_of_overlaps)
 
     # part II
+    print('partI: number of overlapping assignments= ', number_of_overlapping_assignments)
 
 
 if __name__ == '__main__':
