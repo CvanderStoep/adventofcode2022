ca0f2e7c938bc6c7e3ebd9c2bf54381fbf0f4f80
carlo <carlo.vanderstoep@gmail.com>
2022-12-11 11:16:11 +0100

    day 6 completed

--- day6.py
+++ day6.py
@@ -5,6 +5,7 @@
 
 def read_input_file():
     filename = "input/input6.txt"
+    MARKER_LENGTH = 14
 
     with open(filename) as f:
         content = f.read().splitlines()
@@ -13,26 +14,19 @@ def read_input_file():
     start_of_packet_marker = 0
     sequence_found = False
     while not sequence_found:
-        current_data_stream = datastream[start_of_packet_marker: start_of_packet_marker + 4]
+        current_data_stream = datastream[start_of_packet_marker: start_of_packet_marker + MARKER_LENGTH]
 
         # convert the current stream to a set containing only unique letters
 
-        if len(set(current_data_stream)) == 4:  # start found
+        if len(set(current_data_stream)) == MARKER_LENGTH:  # start found
             break
         start_of_packet_marker += 1
 
-        # sequence_found = True
-        # for i in range(1,4):
-        #     next_letter = current_data_stream[i]
-        #     if next_letter in current_data_stream[0:i]:
-        #         sequence_found = False
-        #         break
-        # start_of_packet_marker += 1
-    print(f'partI: start of packet', start_of_packet_marker + 4)
+    print(f'partI: start of packet', start_of_packet_marker + MARKER_LENGTH)
 
 
     # part II
-    # print('partI: number of overlapping assignments= ', number_of_overlapping_assignments)
+    # set markerlength to 4 or 14 for partI and partII respectively
 
 
 if __name__ == '__main__':
