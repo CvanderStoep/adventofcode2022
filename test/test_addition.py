5d50aedf11dd8492f334ab9989d3bfa0f52f36aa
Lex van der Stoep <lexvanderstoep@gmail.com>
2023-02-12 23:33:03 +0100

    Break unittest
    Let's try out our new test pipeline!

--- test/test_addition.py
+++ test/test_addition.py
@@ -3,7 +3,7 @@ import unittest
 
 class AdditionTest(unittest.TestCase):
     def test_addition(self):
-        self.assertEqual(4, 2 + 2)  # add assertion here
+        self.assertEqual(5, 2 + 2)  # add assertion here
 
 
 if __name__ == '__main__':
