84150fb3cb4c26d9fe536278db58d5f0a80e28ba
carlo <carlo.vanderstoep@gmail.com>
2023-02-18 16:37:51 +0100

    day22, restore some merging issues from github

--- day22.py
+++ day22.py
@@ -1,11 +1,18 @@
 import logging
 import re
 
+import numpy as np
+
 def read_input_file(filename):
     """"
     returns a 3D map array[i][j]
     with rows [i] and columns [j]
     returns the path
+    map[i][j]
+    --> j
+    |
+    |i
+    \/
     """
     with open(filename) as f:
         content = f.read().splitlines()
@@ -14,6 +21,18 @@ def read_input_file(filename):
         map.append(list(line))
 
     return map[:-2], line  # returns map + path
+
+def fill_map(map):
+    """right fill map with spaces"""
+    rows = len(map)
+    cols = len(map[0])
+    for c in map:
+        cols = max(cols, len(c))
+
+    for c in map:
+        for _ in range(cols-len(c)):
+            c.append(' ')
+    return map
 
 def convert_path_to_list(path):
     steps = [int(a) for a in re.findall("[0-9]+", path)]
@@ -34,19 +53,32 @@ def evaluate_path(map, position, path, heading):
         else:
             heading = change_heading(rotation=change, heading=heading)
 
+        print(f'{position= }, {heading= }')
     return position, heading
 
 
 def move_position(map, heading, position, steps):
-    match heading:
-        case 0:
-            pass
-        case 90:
-            pass
-        case 180:
-            pass
-        case 270:
-            pass
+    rows = len(map)
+    # cols_r = len(map[r])
+    #  TODO check end of map modulo
+    #  TODO check for spaces/periods 
+
+    for i in range(steps):
+        old_position = position.copy()
+        match heading:
+            case 0:
+                position[0] -= 1
+            case 90:
+                position[1] += 1
+            case 180:
+                position[0] += 1
+            case 270:
+                position[1] -= 1
+
+        if map[position[0]][position[1]] == "#":
+            print('wall encountered')
+            position = old_position
+            break
 
     return position
 
@@ -73,6 +105,7 @@ if __name__ == '__main__':
     )
     filename = "input/input22test.txt"
     map, path = read_input_file(filename)
+    map = fill_map(map)
     path = convert_path_to_list(path)
     logging.info(path)
     start = starting_position(map)
