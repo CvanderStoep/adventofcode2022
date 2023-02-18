4d5ee066133266149cfe8c3bfb5463a661fb7cb2
carlo <carlo.vanderstoep@gmail.com>
2023-02-18 16:39:08 +0100

    testing the workflow

--- test/test_day1.py
+++ test/test_day1.py
@@ -5,7 +5,7 @@ from day1 import compute_part_one, compute_part_two
 
 class Day1Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(compute_part_one("test/input/day1.txt"), 24000)
+        self.assertEqual(compute_part_one("test/input/day1.txt"), 25000)
 
     def test_part_two(self):
         self.assertEqual(compute_part_two("test/input/day1.txt"), 45000)
